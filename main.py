47154a10d17794096d2e8653fc027e84d0fbdb6e
isushkov <sushkov.ilia@gmail.com>
2024-04-16 03:08:48 -0400

    v0.94: remake args - in progress

--- main.py
+++ main.py
@@ -6,59 +6,64 @@ from src.create import create
 from src.analyze import analyze
 from src.run import Run
 
-def create_subparser_for_create_command(subparsers, parent_parser):
-    parser_create = subparsers.add_parser('create', help='Create a new exercise.')
-    create_subparsers = parser_create.add_subparsers(dest='kind', required=True, help='kind for creating exercise.')
-    # Shared arguments for 'random' and 'cover-units' kind
-    shared_parser = argparse.ArgumentParser(add_help=False)
-    shared_parser.add_argument('operations', choices=['plus', 'minus', 'plus-minus', 'plus-minus-roundtrip'], help='Arithmetic operations to use.')
-    shared_parser.add_argument('range', help='The range of possible values for numbers.')
-
-    # 'arithmetic' kind specific
-    parser_arithmetic = create_subparsers.add_parser('arithmetic', parents=[parent_parser], help='Create an arithmetic progression.')
-    parser_arithmetic.add_argument('first', type=int, help='First number of the progression.')
-    parser_arithmetic.add_argument('length', type=int, help='Amount of numbers.')
-    # 'random' kind specific
-    parser_random = create_subparsers.add_parser('random', parents=[shared_parser, parent_parser], help='Generate numbers randomly.')
-    parser_random.add_argument('length', type=int, help='Amount of numbers.')
-    # 'cover-units' kind
-    create_subparsers.add_parser('cover-units', parents=[shared_parser, parent_parser], help='Covering all possible combinations of the category of units.')
-
-def create_subparser_for_runnew_command(subparsers, parent_parser):
-    parser_create = subparsers.add_parser('run-new', help='Create, analyze and run a new exercise.')
-    runnew_subparsers = parser_create.add_subparsers(dest='kind', required=True, help='kind for creating exercise.')
-    # Shared arguments for 'random' and 'cover-units' kind
-    shared_parser = argparse.ArgumentParser(add_help=False)
-    shared_parser.add_argument('operations', choices=['plus', 'minus', 'plus-minus', 'plus-minus-roundtrip'], help='Arithmetic operations to use.')
-    shared_parser.add_argument('range', help='The range of possible values for numbers.')
-
-    # 'arithmetic' kind specific
-    parser_arithmetic = runnew_subparsers.add_parser('arithmetic', parents=[parent_parser], help='Create an arithmetic progression.')
-    parser_arithmetic.add_argument('first', type=int, help='First number of the progression.')
-    parser_arithmetic.add_argument('length', type=int, help='Amount of numbers.')
-    # 'random' kind specific
-    parser_random = runnew_subparsers.add_parser('random', parents=[shared_parser, parent_parser], help='Generate numbers randomly.')
-    parser_random.add_argument('length', type=int, help='Amount of numbers.')
-    # 'cover-units' kind
-    runnew_subparsers.add_parser('cover-units', parents=[shared_parser, parent_parser], help='Covering all possible combinations of the category of units.')
-
 def main():
     parser = argparse.ArgumentParser(description='Soroban exercise management system.')
-    subparsers = parser.add_subparsers(dest='command', required=True, help='Command to perform.')
-    parent_parser = argparse.ArgumentParser(add_help=False)
-    parent_parser.add_argument('name', nargs='?', help='Name for the new exercise.')
-    # 'create' command
-    create_subparser_for_create_command(subparsers, parent_parser)
+    # <command>
+    command = parser.add_subparsers(dest='command', required=True, help='Command to perform.')
+    create  = command.add_parser('create', help='Create a new exercise.')
+    analyze = command.add_parser('analyze', help='Analyze an exercise.')
+    run     = command.add_parser('run', help='Run an existing exercise.')
+    run_new = command.add_parser('run-new', help='Create, analyze and run a new exercise.')
+    # create <kind>
+    kind = create.add_subparsers(dest='kind', required=True, help='Type of exercise.')
+    arithmetic = kind.add_parser('arithmetic', help='An arithmetic progression.')
+    ramdom     = kind.add_parser('random', help='Generate numbers randomly.')
+    cover      = kind.add_parser('cover-units', help='Covering all possible combinations of the category of units.')
+    # create arithmetic [first,diff,length] (name)
+    arithmetic.add_argument('first',  type=float, help='First number of the progression.')
+    arithmetic.add_argument('diff',   type=float, help='The difference between the numbers.')
+    arithmetic.add_argument('length', type=int, help='The length of the progression.')
+    # arithmetic.add_argument('name')
+
+    # create random [params] (name)
+    ramdom.add_argument('params', type=str, help='Amount of numbers.')
+    # ramdom.add_argument('name')
+
+    # create cover [params] (name)
+    # analyze [filiname]
+    # run <style> <mode> [filiname]
+    # run-new <style> <mode> arithmetic [first,diff,length] (name)
+    # run-new <style> <mode> random [params] (name)
+    # run-new <style> <mode> cover [params] (name)
+
+
+
+    # 'cover-units' kind
+
     # 'analyze' command
-    parser_analyze = subparsers.add_parser('analyze', help='Analyze an exercise.')
-    parser_analyze.add_argument('path', help='Path to the file.')
+    analyze.add_argument('path', help='Path to the file.')
+
     # 'run' command
-    parser_run = subparsers.add_parser('run', help='Run an existing exercise.')
-    parser_run.add_argument('path', help='Path to the file.')
+    run.add_argument('path', help='Path to the file.')
+
     # 'run-new' command
-    create_subparser_for_runnew_command(subparsers, parent_parser)
+    runnew_subparsers = run_new.add_subparsers(dest='kind', required=True, help='kind for creating exercise.')
+
+    # Reusing common parser for 'run-new' kinds
+    # 'arithmetic' kind specific
+
+    # 'random' kind specific
+    ramdom = runnew_subparsers.add_parser('random', parents=[parent_parser, common_parser], help='Generate numbers randomly.')
+    ramdom.add_argument('length', type=int, help='Amount of numbers.')
+
+    # 'cover-units' kind
+    runnew_subparsers.add_parser('cover-units', parents=[parent_parser, common_parser], help='Covering all possible combinations of the category of units.')
+
+
 
     args = parser.parse_args()
+    print(args)  # Display parsed arguments
+    exit()
     if args.command == 'create':
         path = create(args)
         analyze(path)
