e9e65b393ea1751f6db02b1710a87aac3b284c02
isushkov <sushkov.ilia@gmail.com>
2024-04-19 03:41:41 -0400

    v0.95: init "create random"

--- src/create.py
+++ src/create.py
@@ -4,6 +4,8 @@ from src.helpers.cmd import Cmd as cmd
 from src.params import parse_params
 import random
 from decimal import Decimal, getcontext, ROUND_HALF_UP
+import ast
+import operator
 
 
 # common
@@ -11,7 +13,7 @@ def create(args):
     print(c_center(cz(f' [y]CREATING ({args.kind}) '), 94, '=', 'x'))
     if args.kind == 'arithmetic':
         return create_arithmetic(args.path, args.first, args.diff, args.length)
-    params = parse_params(args.params)
+    params = parse_params(args.params, args.kind)
     if args.kind == 'random':
         return create_random(args.path, params)
     if args.kind == 'cover-units':
@@ -23,9 +25,10 @@ def num2str(num):
         if num == num.to_integral_value():
             return str(num.quantize(Decimal('1')))
         return str(num)
-    if num == int(num):
+    try:
         return str(int(num))
-    return str(num)
+    except ValueError:
+        return str(num)
 def do_math(operand, x,y):
     map_operations = {
         '+': lambda x, y: x + y,
@@ -37,12 +40,44 @@ def do_math(operand, x,y):
         )[1]
     }
     return map_operations[operand](x,y)
-def parans2path(kind, params=False):
-    # if params:
-    # else:
-    #     p = ''
 
-    return f'./data/{kind}{p}.txt'
+def safe_eval(expr):
+    operators = {
+        ast.Add: operator.add, ast.Sub: operator.sub,
+        ast.Mult: operator.mul, ast.Div: operator.truediv,
+        ast.Pow: operator.pow, ast.BitXor: operator.xor,
+        ast.USub: operator.neg
+    }
+    def eval_(node):
+        if isinstance(node, ast.Num):  # <number>
+            return node.n
+        elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
+            return operators[type(node.op)](eval_(node.left), eval_(node.right))
+        elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+            return operators[type(node.op)](eval_(node.operand))
+        else:
+            raise TypeError(node)
+    return eval_(ast.parse(expr, mode='eval').body)
+def params2path(kind, params=False):
+    if not params:
+        return f'./data/{kind}.txt'
+    name = f'./data/{kind}'
+    for seq in params:
+        start_number, operands, range_params, length = seq['required'].values()
+        is_negative, is_roundtrip, float_params = seq['optional'].values()
+        precision = float_params['precision']
+        probability = float_params['probability']
+        m = {'+':'p', '-':'m', '*':'M', '/':'D'}
+        start_number = str(start_number)
+        operands = ''.join([f'{m[k]}{v}' for k,v in operands.items()])
+        range_params = 'x'.join(str(i) for i in range_params)
+        length = length if length else 0
+        float_params = f'dec{precision}x{probability}' if precision else 'dec0'
+        negative = f'neg{1 if is_negative else 0}'
+        roundtrip = f'rnd{1 if is_roundtrip else 0}'
+        name += f'_{start_number}-{operands}-{range_params}-{length}_{float_params}-{negative}-{roundtrip}'
+    name += '.txt'
+    return name
 def save_file(path, data):
     cmd.run('mkdir -p ./data')
     fo.str2txt(data, path)
@@ -54,11 +89,11 @@ def create_arithmetic(path, first, diff, length):
     print(cz(f'[g]first element:[c] {first}[g], diff: [c]{diff}, length: [c]{length}'))
     # data
     numbers = generate_arithmetic(first, diff, length)
-    expresion = get_arithmetic_expresion(numbers)
+    expression = get_arithmetic_expresion(numbers)
     total = num2str(sum(numbers, Decimal(0)))
-    data = f'{expresion} ={total}'
+    data = f'{expression} ={total}'
     # save
-    path = path if path else parans2path(f'arithmetic_{first}_{diff}_{length}')
+    path = path if path else params2path(f'arithmetic_{first}_{diff}_{length}')
     save_file(path, data)
     return path
 def generate_arithmetic(first, diff, length):
@@ -66,7 +101,7 @@ def generate_arithmetic(first, diff, length):
     diff = Decimal(str(diff))
     return [first + i * diff for i in range(length)]
 def get_arithmetic_expresion(numbers):
-    expresion = ''
+    expression = ''
     for num in numbers:
         operand = '+' if num >= 0 else '-'
         if num == numbers[0]:
@@ -75,29 +110,30 @@ def get_arithmetic_expresion(numbers):
                 operand = ''
         else:
             pfx = ' '
-        expresion += f'{pfx}{operand}{num2str(abs(num))}'
-    return expresion
+        expression += f'{pfx}{operand}{num2str(abs(num))}'
+    return expression
 
 # random
 def create_random(path, params):
     # data
-    print(cz(f'[g]params:[c]'))
-    sequence = ''
-    for i,sequence_params in enumerate(params):
-        required = sequence_params['required']
-        optional = sequence_params['optional']
-        print(cz(f'  [g]sequence_{i}:[c]'))
-        print(cz(f'    [r]required:[c] {required}'))
-        print(cz(f'    [y]optional:[c] {optional}'))
-        sequence += create_random_sequence(required, optional)
+    expression = ''
+    for i,seq in enumerate(params):
+        required = seq['required']
+        optional = seq['optional']
+        print(cz(f'[g]sequence_{i}:[c]'))
+        print(cz(f'  [r]required:[c] {required}'))
+        print(cz(f'  [y]optional:[c] {optional}'))
+        expression += create_random_sequence(required, optional)
     # save
-    path = path if path else parans2path('random', params)
+    path = path if path else params2path('random', params)
+    total = safe_eval(expression)
+    data = f'{expression} = {total}'
+    expression
     save_file(path, data)
     return path
-
 def create_random_sequence(required, optional):
     start_number_param, operands, range_params, max_length = required.values()
-    is_wnegative, is_roundtrip, float_params = optional.values()
+    is_negative, is_roundtrip, float_params = optional.values()
     # start_number
     if start_number_param == 'r':
         start_number = create_random_number(range_params, float_params)
@@ -108,8 +144,8 @@ def create_random_sequence(required, optional):
     total = Decimal(str(start_number))
     length = 1
     while length <= max_length:
-        operand, number, total = create_operation(total,operands,range_params,
-                                                  float_params, total, is_wnegative)
+        operand, number, total = create_operation(total, operands, range_params,
+                                                  float_params, total, is_negative)
         sequence += ' ' + operand + num2str(number)
         length += 1
     # roundtrip
@@ -117,22 +153,25 @@ def create_random_sequence(required, optional):
         elements = sequence.split()
         elements.pop(0)
         elements.reverse()
-        round_sequence = ' '.join([replace_operand(el) for el in elements])
+        round_sequence = ' '.join([switch_operand(el) for el in elements])
         sequence += round_sequence
     return sequence
-
-def create_operation(start_number, operands, range_params, float_params, total, is_wnegative):
-    backup = (start_number, operands, range_params, float_params, total, is_wnegative)
+def create_operation(start_number, operands, range_params, float_params, total, is_negative):
+    backup = (start_number, operands, range_params, float_params, total, is_negative)
     operand = choose_operand(operands)
     number = Decimal(str(create_random_number(range_params, float_params)))
     total = do_math(operand, total, number)
-    # check conditions
     is_ok = True
-    if not is_wnegative:
-        if start_number >= 0 and total < 0:
-            is_ok = False
-        if start_number < 0 and total < start_number:
-            is_ok = False
+    if not is_negative:
+        if start_number >= 0 and total < 0:           is_ok = False
+        if start_number < 0 and total < start_number: is_ok = False
+        if len(operands) == 1 and operand == '-' and total - range_params[0] < 0:
+            print(cz('[r]FAIL:[c] The sequence [r]was not created[c] due to conflicting parameters:'))
+            print(cz('[y]FAIL:[c]   - Negative numbers are not allowed.'))
+            print(cz('[y]FAIL:[c]   - Only "-" operands are used.'))
+            print(cz('[y]FAIL:[c]   - The current result minus the minimum possible value is less than zero.'))
+            print(cz('[y]FAIL:[c]   - The limit has been reached.'))
+            exit(1)
     if not is_ok:
         operand, number, total = create_operation(*backup)
     return operand, number, total
@@ -140,11 +179,6 @@ def choose_operand(operands):
     operations, weights = zip(*operands.items())
     operand = random.choices(operations, weights=weights, k=1)[0]
     return operand
-def create_random_number(range_params, float_params):
-    if float_params['precision']:
-        number = random.uniform(*range_params)
-        return format(number, f".{float_params['precision']}f")
-    return random.randint(*range_params)
 def create_random_number(range_params, float_params):
     # должны ли мы генерировать float
     if float_params['precision']:
@@ -154,7 +188,7 @@ def create_random_number(range_params, float_params):
             return format(number, f".{float_params['precision']}f")
         return random.randint(*range_params)
     return random.randint(*range_params)
-def replace_operand(op):
+def switch_operand(op):
     return {'+': '-', '-': '+', '*': '/', '/': '*'}.get(op, op)
 
 # cover-units
@@ -166,12 +200,12 @@ def create_cover(operations_kind, digits_range, name):
     numbers = get_cover_units(digits_range)
     if operations_kind == 'plus':
         total = sum(numbers)
-        expresion = get_expresion(numbers, operations_kind)
+        expression = get_expresion(numbers, operations_kind)
     if operations_kind == 'minus':
         numbers.append(sum(numbers))
         numbers.reverse()
         total = numbers.pop(-1)
-        expresion = get_expresion(numbers, operations_kind)
+        expression = get_expresion(numbers, operations_kind)
     # TODO: plus-minus
     if operations_kind == 'plus-minus':
         print(cz('[y]TODO:[c] cover-units plus-minus...'))
@@ -181,9 +215,9 @@ def create_cover(operations_kind, digits_range, name):
         numbers.reverse()
         total = numbers.pop(0)
         expresion_minus = get_expresion(numbers, 'minus')
-        expresion = f'{expresion_plus} - {expresion_minus}'
+        expression = f'{expresion_plus} - {expresion_minus}'
     # save
-    data = f'{expresion} ={total}'
+    data = f'{expression} ={total}'
     save_file(path, data)
     return path
 def get_cover_units(digits_range):
@@ -221,7 +255,7 @@ def get_expresion(numbers, operations_kind):
     if operations_kind == 'plus-minus':
         operators = ['+', '-']
         summ = numbers[0]
-        expresion = ''
+        expression = ''
         for num in numbers:
             if num == numbers[0]:
                 continue
@@ -230,5 +264,5 @@ def get_expresion(numbers, operations_kind):
             if summ < 0:
                 choice = '+'
                 summ = summ + num
-            expresion += f' {choice}{num}'
-        return expresion
+            expression += f' {choice}{num}'
+        return expression
--- src/params.py
+++ src/params.py
@@ -4,60 +4,63 @@ from src.helpers.colors import *
 from decimal import Decimal, getcontext
 
 # sequences
-def parse_params(params):
+def parse_params(params, kind):
     sequences = re.split(r'\s+', params.strip())
     parsed_sequences = []
     for seq in sequences:
         is_first_seq = True if seq == sequences[0] else False
-        parsed_sequences.append(parse_sequence(seq.strip(), is_first_seq))
+        parsed_sequences.append(parse_sequence(seq.strip(), kind, is_first_seq))
     return parsed_sequences
-def parse_sequence(sequence, is_first_seq):
+def parse_sequence(sequence, kind, is_first_seq):
     if ':' in sequence:
         required_part, optional_part = sequence.split(':')
     else:
         required_part, optional_part = sequence, ''
-    required = parse_required(required_part, is_first_seq)
+    required = parse_required(required_part, kind, is_first_seq)
     optional = parse_optional(optional_part)
     # hooks
     check_precision(required['start_number'], optional['float'])
-    check_range_precision(required['range'], optional['float'])
-
+    check_range_precision(required['range'], optional['float']['precision'])
     return {'required': required, 'optional': optional}
+
 # required
-def parse_required(required_str, is_first_seq):
+def parse_required(required_str, kind, is_first_seq):
     parts = required_str.split(',')
-    if is_first_seq:
-        if len(parts) == 4:
-            start_number, operands, value_range, length = parts
-        elif len(parts) == 3:
-            start_number, operands, value_range = parts
-            length = 0
-        else:
-            print(cz(f'[r]Invalid params for required options:[c] "{required_str}"'))
-            exit(1)
-    else:
-        start_number = 0
-        if len(parts) == 3:
-            operands, value_range, length = parts
-        elif len(parts) == 2:
-            operands, value_range = parts
-            length = 0
-        else:
-            print(cz(f'[r]Invalid params for required options:[c] "{required_str}"'))
-            exit(1)
-    value_range = parse_range(value_range)
-    operands = parse_operands(operands)
-    length = int(length)
-    return {
-        'start_number': start_number,
-        'operands': operands,
-        'range': value_range,
-        'length': length
+    expected_lengths = {
+        'random': (4 if is_first_seq else 3),
+        'cover':  (3 if is_first_seq else 2)
     }
+    expected_length = expected_lengths.get(kind, None)
+    if len(parts) != expected_length:
+        params_error(f'Expected {expected_length} parts, got {len(parts)}: "{required_str}"')
+    if not is_first_seq: parts.insert(0, None) # fill start_number
+    if kind == 'cover':  parts.append(None)    # fill length
+    start_number, operands, value_range, length = parts
+    return {
+        'start_number': parse_start_number(start_number),
+        'operands':     parse_operands(operands),
+        'range':        parse_range(value_range),
+        'length':       parse_length(length)
+    }
+def parse_start_number(start_number):
+    if start_number is None: return start_number
+    if start_number == 'r':  return start_number
+    try:
+        return int(start_number)
+    except ValueError:
+        try:
+            return float(start_number)
+        except ValueError:
+            params_error(f'Incorrect start number - "{start_number}".')
+def parse_operands(op_str):
+    ops = re.findall(r"([+\-*/])(\d*)", op_str)
+    operands = {}
+    for op, priority in ops:
+        operands[op] = int(priority) if priority else 1
+    return operands
 def parse_range(range_str):
     if range_str.count('-') > 1:
-        print(cz(f'[r]ParamsError:[c] The range string "{range_str}" contains more than one dash ("-").'))
-        exit(1)
+        params_error(f'The range string "{range_str}" contains more than one dash ("-").')
     start, end = map(int, range_str.split('-'))
     if end < 1:
         print(cz('[y]Note:[c] Minimal value for the second number in the range of digits is "1".'))
@@ -68,19 +71,20 @@ def parse_range(range_str):
         print(cz('[y]Note:[c] The initial value was replaced to "1".'))
         start = 1
     return (start, end)
-def parse_operands(op_str):
-    ops = re.findall(r"([+\-*/])(\d*)", op_str)
-    operands = {}
-    for op, priority in ops:
-        operands[op] = int(priority) if priority else 1
-    return operands
+def parse_length(length):
+    if length is None:
+        return length
+    try:
+        return int(length)
+    except ValueError:
+        params_error(f'Incorrect length - "{length}".')
 
 # optional
 def parse_optional(optional_str):
     options = {}
-    options['is_wnegative'] = True if 'n' in optional_str else False
+    options['is_negative']  = True if 'n' in optional_str else False
     options['is_roundtrip'] = True if '<' in optional_str else False
-    float_match = re.search(r'\.(\d+)%?(\d+)?', optional_str)
+    float_match = re.search(r'\.(\d+)(?:%(\d+))?', optional_str)
     if float_match:
         precision = int(float_match.group(1))
         probability = int(float_match.group(2)) if float_match.group(2) else 10
@@ -100,12 +104,19 @@ def check_precision(start_number, float_params):
     except InvalidOperation:
         post_decimal_digits = 0
     if post_decimal_digits > precision:
-        print(cz(f'[r]ParamsError:[c] the number of decimal places in start number ({start_number}) exceeds the specified precision ({precision}).'))
-        exit(1)
+        params_error(f'the number of decimal places in start number ({start_number}) exceeds the specified precision ({precision}).')
     return True
-def check_range_precision(required['range'], optional['float']):
-    if not optional
-    # range не может быть дробным если нет optional['float']
+def check_range_precision(range_params, precision):
+    converter = float if not precision else int
+    try:
+        converted_range = tuple(converter(x) for x in range_params)
+        return converted_range
+    except ValueError:
+        params_error(f'invalid range: {range_params}')
+
+def params_error(msg):
+    print(cz(f'[r]ParamsError:[c] {msg}'))
+    exit(1)
 
 def test():
     params_list = [
